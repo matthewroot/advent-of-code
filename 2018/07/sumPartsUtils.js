7bacc8e5184affe167e3f5025cef4e21fd067736
Matthew Root <root.matthew@gmail.com>
2019-02-05 12:45:56 -0800

    Add final working solution for day 7 part 1

--- 2018/07/sumPartsUtils.js
+++ 2018/07/sumPartsUtils.js
@@ -33,10 +33,96 @@ function parseInput(filepath) {
   return stepMapping;
 }
 
-// Determines the instruction order based on the group of knownSteps and the known firstStep
-// Return: [String] Order of instructions
-function determineOrder(knownSteps, firstStep) {
-  //
+// Creates a mapping of step to its subsequent steps and prerequisite steps
+// Return [Object] Maps a step value ('A') to a Step object
+function processSteps(steps) {
+  let knownSteps = {};
+
+  steps.forEach(step => {
+    const [prereqValue, nextValue] = step;
+    let prereqStep = knownSteps[prereqValue];
+    let nextStep = knownSteps[nextValue];
+
+    if (!prereqStep) {
+      prereqStep = new Step(prereqValue);
+      knownSteps[prereqValue] = prereqStep;
+    }
+
+    if (!nextStep) {
+      nextStep = new Step(nextValue);
+      knownSteps[nextValue] = nextStep;
+    }
+
+    prereqStep.addNextStep(nextStep);
+    nextStep.addPrereqStep(prereqStep);
+  });
+
+  return knownSteps;
 }
 
-module.exports = { Step, parseInput, determineOrder };
+// Determines the instruction order based on the group of knownSteps
+// Return: [String] Order of instructions
+function determineOrder(knownSteps) {
+  let availableNextSteps = getInitialSteps(knownSteps);
+  let instructionOrder = '';
+
+  while (availableNextSteps.length > 0) {
+    let currentStep = knownSteps[availableNextSteps.shift()];
+
+    if (prereqsMet(currentStep, instructionOrder)) {
+      instructionOrder = instructionOrder.concat(currentStep.value);
+
+      currentStep.nextSteps.forEach(nextStep => {
+        if (!availableNextSteps.includes(nextStep.value)) {
+          availableNextSteps.push(nextStep.value);
+        }
+      });
+
+      availableNextSteps.sort();
+    } else {
+      availableNextSteps.push(currentStep.value);
+    }
+  }
+
+  return instructionOrder;
+}
+
+// Initial steps will have an empty prereqs array
+// Return: array of step values
+function getInitialSteps(steps) {
+  let initialSteps = [];
+
+  for (const key in steps) {
+    if (steps.hasOwnProperty(key)) {
+      const step = steps[key];
+
+      if (step.prereqs.length === 0) {
+        initialSteps.push(step.value);
+      }
+    }
+  }
+
+  initialSteps.sort();
+
+  return initialSteps;
+}
+
+// Prereqs have been met if the prereqs array is empty or all prereqs are part of
+// the current instructions string
+function prereqsMet(step, instructions) {
+  if (step.prereqs.length === 0) {
+    return true;
+  }
+
+  let completedPrereqs = step.prereqs.filter(prereq =>
+    instructions.includes(prereq.value)
+  );
+
+  if (completedPrereqs.length === step.prereqs.length) {
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = { Step, parseInput, determineOrder, processSteps };
--- 2018/07/sumPartsUtils.test.js
+++ 2018/07/sumPartsUtils.test.js
@@ -1,7 +1,17 @@
-const { parseInput, determineOrder } = require('./sumPartsUtils');
+const {
+  Step,
+  parseInput,
+  determineOrder,
+  processSteps,
+} = require('./sumPartsUtils');
+
+let parsedInput;
+
+beforeAll(() => {
+  parsedInput = parseInput('./2018/07/testInput.txt');
+});
 
 test('parseInput outputs an array of step mapping arrays', () => {
-  const parsedInput = parseInput('./2018/07/testInput.txt');
   const stepMapping = [
     ['C', 'A'],
     ['C', 'F'],
@@ -15,6 +25,54 @@ test('parseInput outputs an array of step mapping arrays', () => {
   expect(parsedInput).toEqual(stepMapping);
 });
 
-test('determineOrder returns the ordered set of instructions in a string', () => {
-  //
+test('processSteps returns an object mapping step value to next steps and prereqs', () => {
+  const processedSteps = processSteps(parsedInput);
+  const stepMapping = createStepMapping();
+
+  expect(processedSteps).toEqual(stepMapping);
 });
+
+test('determineOrder returns the ordered set of instructions in a string', () => {
+  const stepMapping = createStepMapping();
+  const order = determineOrder(stepMapping);
+
+  expect(order).toBe('CABDFE');
+});
+
+function createStepMapping() {
+  let stepA = new Step('A');
+  let stepB = new Step('B');
+  let stepC = new Step('C');
+  let stepD = new Step('D');
+  let stepE = new Step('E');
+  let stepF = new Step('F');
+
+  stepA.addNextStep(stepB);
+  stepA.addNextStep(stepD);
+  stepA.addPrereqStep(stepC);
+
+  stepB.addNextStep(stepE);
+  stepB.addPrereqStep(stepA);
+
+  stepC.addNextStep(stepA);
+  stepC.addNextStep(stepF);
+
+  stepD.addNextStep(stepE);
+  stepD.addPrereqStep(stepA);
+
+  stepE.addPrereqStep(stepB);
+  stepE.addPrereqStep(stepD);
+  stepE.addPrereqStep(stepF);
+
+  stepF.addNextStep(stepE);
+  stepF.addPrereqStep(stepC);
+
+  return {
+    A: stepA,
+    B: stepB,
+    C: stepC,
+    D: stepD,
+    E: stepE,
+    F: stepF,
+  };
+}
